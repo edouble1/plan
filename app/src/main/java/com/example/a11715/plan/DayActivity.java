0b888e247d3d51546697e8626bea57d342b29464
edouble <1171574037@qq.com>
2018-07-10 21:29:47 +0800

    First commit.

---
+++ app/src/main/java/com/example/a11715/plan/DayActivity.java
@@ -0,0 +1,13 @@
+package com.example.a11715.plan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+    }
+}
